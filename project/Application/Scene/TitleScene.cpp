83020032e219692f0782bb9f91245a42602b9e9e
nakanuma <k023g0121@g.neec.ac.jp>
2024-10-20 23:34:01 +0900

    奥へ移動しても影が映るよう修正

--- project/Application/Scene/TitleScene.cpp
+++ project/Application/Scene/TitleScene.cpp
@@ -36,7 +36,8 @@ void TitleScene::Initialize()
 	lightManager->Initialize();
 	// プレイヤーの手に割り当てる丸影を有効化
 	lightManager->spotLightsCB_.data_->spotLights[0].isActive = true;
-	lightManager->spotLightsCB_.data_->spotLights[0].intensity = 6.0f;
+	lightManager->spotLightsCB_.data_->spotLights[0].decay = 0.5f;
+	lightManager->spotLightsCB_.data_->spotLights[0].distance = 60;
 
 	camera = SceneManager::GetInstance()->GetCamera();
 
