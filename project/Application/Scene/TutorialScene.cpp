85df83fc33dd0f605da8ac498ad4795d71d48e88
katajima <k023g1078@g.neec.ac.jp>
2024-10-22 16:58:55 +0900

    Release時チュートリアルシーンにImGuiが出ないように修正

--- project/Application/Scene/TutorialScene.cpp
+++ project/Application/Scene/TutorialScene.cpp
@@ -240,7 +240,7 @@ void TutorialScene::Draw()
 #pragma endregion
 #ifdef _DEBUG
 	GlobalVariables::getInstance()->Update();
-#endif // _DEBUG
+
 
 	ImGui::Begin("Camera");
 
@@ -263,6 +263,7 @@ void TutorialScene::Draw()
 	ImGui::InputFloat("CurrentTime",&currentTime,0.0f,0.0f,"%.1f",ImGuiInputTextFlags_ReadOnly);
 
 	ImGui::End();
+#endif // _DEBUG
 
 	// ImGuiの内部コマンドを生成する
 	ImguiWrapper::Render(dxBase->GetCommandList());
